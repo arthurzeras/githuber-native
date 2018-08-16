73ee66cdc3a9e7976055bc4e6c5d6aea9199064a
arthurzeras <arthurolvmorais@gmail.com>
2018-08-16 13:32:57 -0300

    Populando a página welcome e criando arquivo de estilos

--- src/pages/welcome/index.js
+++ src/pages/welcome/index.js
@@ -1,5 +1,27 @@
 import React from 'react'
-import { View } from 'react-native'
+import styles from './styles'
+import { View, Text, TextInput, TouchableOpacity } from 'react-native'
 
-const Welcome = () => <View/>
+const Welcome = () => (
+  <View style={styles.container}>
+    <Text style={styles.title}>Bem-vindo</Text>
+    <Text style={styles.text}>
+      Para continuar, informe o seu usuário do Github.
+    </Text>
+
+    <View style={styles.form}>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        placeholder="Digite o seu usuário"
+        underlineColorAndroid="rgba(0, 0, 0, 0)"
+      />
+
+      <TouchableOpacity style={styles.button} onPress={() => {}}>
+        <Text style={styles.buttonText}>Prosseguir</Text>
+      </TouchableOpacity>
+    </View>
+  </View>
+)
 export default Welcome
