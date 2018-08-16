38a155276970a33667d612fe9c49dfb4f87269b5
arthurzeras <arthurolvmorais@gmail.com>
2018-08-16 13:44:39 -0300

    Criando estilos do welcome

--- src/pages/welcome/styles.js
+++ src/pages/welcome/styles.js
@@ -1,8 +1,47 @@
 import { StyleSheet } from 'react-native'
+import { colors, metrics } from 'styles'
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    padding: metrics.basePadding * 2,
+    backgroundColor: colors.secondary
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: colors.white
+  },
+  text: {
+    marginTop: 5,
+    fontSize: 14,
+    color: colors.light,
+    textAlign: 'center'
+  },
+  form: {
+    marginTop: metrics.baseMargin * 2
+  },
+  input: {
+    height: 44,
+    backgroundColor: colors.white,
+    borderRadius: metrics.baseRadius,
+    paddingHorizontal: metrics.basePadding
+  },
+  button: {
+    height: 44,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: metrics.baseMargin,
+    backgroundColor: colors.primary,
+    borderRadius: metrics.baseRadius
+  },
+  buttonText: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: colors.white
   }
 })
 
