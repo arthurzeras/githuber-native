c1b8bcbc8b83f84e606fc3d25c4aac395152a96d
arthurzeras <arthurolvmorais@gmail.com>
2018-08-17 15:35:48 -0300

    Listando repositórios do usuario

--- src/pages/repositories/index.js
+++ src/pages/repositories/index.js
@@ -1,15 +1,62 @@
-import { View } from 'react-native'
+import styles from './styles'
+import api from 'services/api'
 import React, { Component } from 'react'
 import Icon from 'react-native-vector-icons/FontAwesome'
+import {
+  Text,
+  View,
+  FlatList,
+  AsyncStorage,
+  ActivityIndicator
+} from 'react-native'
 
 export default class Repositories extends Component {
   static navigationOptions = {
     tabBarIcon: ({ tintColor }) => <Icon name="list-alt" size={20} color={tintColor} />
   }
 
+  state = {
+    data: [],
+    loading: true
+  }
+
+  componentDidMount() {
+    this.loadRepositories()
+  }
+
+  loadRepositories = async () => {
+    const username = await AsyncStorage.getItem('@Githuber:username')
+    const res = await api.get(`/users/${username}/repos`)
+
+    this.setState({
+      data: res.data,
+      loading: false
+    })
+  }
+
+  renderList = () => (
+    <FlatList
+      data={this.state.data}
+      keyExtractor={item => String(item.id)}
+      renderItem={this.renderListItem}
+    >
+
+    </FlatList>
+  )
+
+  renderListItem = ({ item }) => (
+    <Text>{item.full_name}</Text>
+  )
+
   render () {
     return (
-      <View/>
+      <View style={styles.container}>
+        {
+          this.state.loading
+            ? <ActivityIndicator style={styles.loading}/>
+            : this.renderList()
+        }
+      </View>
     )
   }
 }
