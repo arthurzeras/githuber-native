da9f68248ffd4a939501fbeeff3305ac35b4bea7
arthurzeras <arthurolvmorais@gmail.com>
2018-08-20 14:56:41 -0300

    Estilizando input de pesquisa

--- src/pages/favorites/index.js
+++ src/pages/favorites/index.js
@@ -1,7 +1,7 @@
 import styles from './styles'
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome'
+import { View, Text, TextInput, TouchableOpacity } from 'react-native'
 
 export default class Favorites extends Component {
   static navigationOptions = {
@@ -9,6 +9,29 @@ export default class Favorites extends Component {
   }
 
   render() {
-    return <View/>
+    return (
+      <View style={styles.container}>
+        <View style={styles.form}>
+          <TextInput
+            autoCorrect={false}
+            style={styles.input}
+            autoCapitalize="none"
+            placeholder="usuário/repositório"
+            underlineColorAndroid="transparent"
+            placeholderTextColor="rgba(255, 255, 255, .4)"
+          />
+
+          <TouchableOpacity
+            onPress={() => {}}
+            style={styles.inputButton}
+            activityOpacity={0.6}
+          >
+            <Text style={styles.buttonText}>
+              <Icon name="plus" size={20} color="#fff"/>
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
   }
 }
--- src/pages/favorites/styles.js
+++ src/pages/favorites/styles.js
@@ -1,5 +1,28 @@
+import { colors, metrics } from 'styles'
 import { StyleSheet } from 'react-native'
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  form: {
+    height: 50,
+    padding: 5,
+    alignSelf: 'stretch',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: colors.primary,
+    paddingHorizontal: metrics.basePadding
+  },
+  input: {
+    fontSize: 16,
+    color: colors.white
+  },
+  inputButton: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
 
 export default styles
