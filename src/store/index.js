ca784919bab7042e684256229ce7ea5b5931c757
arthurzeras <arthurolvmorais@gmail.com>
2018-08-20 16:45:55 -0300

    Configurando redux-saga

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,14 @@
 import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
 import reducers from './reducers'
+import sagas from './sagas'
 
-const middlewares = []
+const sagaMiddleware = createSagaMiddleware()
+const middlewares = [
+  sagaMiddleware
+]
 const createAppropriateStore = __DEV__ ? console.tron.createStore : createStore
 const store = createAppropriateStore(reducers, applyMiddleware(...middlewares))
+sagaMiddleware.run(sagas)
 
 export default store
