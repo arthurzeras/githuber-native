fa9e371079666a1ebd1dc989da1b9e44e43d79cc
arthurzeras <arthurolvmorais@gmail.com>
2018-08-20 16:29:42 -0300

    Criando componente para listar os favoritos

---
+++ src/pages/favorites/styles.js
@@ -0,0 +1,15 @@
+import { general, metrics, colors } from 'styles'
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  empty: {
+    fontSize: 15,
+    alignSelf: 'center',
+    marginTop: metrics.basePadding
+  }
+})
+
+export default styles
